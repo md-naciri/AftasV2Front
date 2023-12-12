4a675c40bb4e3492c12e5968fff424bfc9674f9d
md_naciri <med.ar.naciri@gmail.com>
2023-12-12 19:36:50 +0100

    create methods to get all competions

--- src/app/modules/competitions/components/competitions/competitions.component.ts
+++ src/app/modules/competitions/components/competitions/competitions.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { CompetitionsService } from '../../services/competitions.service';
 
 @Component({
   selector: 'app-competitions',
   templateUrl: './competitions.component.html',
   styleUrl: './competitions.component.scss'
 })
+
 export class CompetitionsComponent {
 
+  constructor(private competitionService:CompetitionsService) { }
+
+  getAllCompetitions(){
+    this.competitionService.getAllCompetitions().subscribe(
+      (response) => {
+        console.log(response);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+  ngOnInit() {
+    this.getAllCompetitions();
+  }
 }
--- src/app/modules/competitions/services/competitions.service.ts
+++ src/app/modules/competitions/services/competitions.service.ts
@@ -1,9 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from '../../../../environments/environment';
 
+// singlton pattern is used here 
 @Injectable({
   providedIn: 'root'
 })
 export class CompetitionsService {
 
-  constructor() { }
+  constructor(private httpClient:HttpClient) { }
+
+  getAllCompetitions(){
+    return this.httpClient.get(environment.apiURL+'competitions');
+  }
 }
