3bf371a933b836fc9cfeac9853f62bb6a24d3f3d
md_naciri <med.ar.naciri@gmail.com>
2023-12-13 13:36:11 +0100

    add all necessary method and props in add fish component

--- src/app/modules/fishs/components/add-fish/add-fish.component.ts
+++ src/app/modules/fishs/components/add-fish/add-fish.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import { FishService } from '../../services/fish.service';
+import { MatDialogRef } from '@angular/material/dialog';
 @Component({
   selector: 'app-add-fish',
   templateUrl: './add-fish.component.html',
@@ -7,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class AddFishComponent {
 
+  errorMessage: string='';
+  successMessage: string='';
+
+  constructor(
+    private fishService:FishService,
+    private dialogRef: MatDialogRef<AddFishComponent>
+  ) { }
+  
+  closeLevelModal(){
+    this.dialogRef.close();
+  }
 }
