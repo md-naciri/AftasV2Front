8f2062ba1225b2fd83ab0c569ac2fc88d2586116
md_naciri <med.ar.naciri@gmail.com>
2023-12-13 13:51:51 +0100

    display all levels in drop down list

--- src/app/modules/fishs/components/add-fish/add-fish.component.ts
+++ src/app/modules/fishs/components/add-fish/add-fish.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FishService } from '../../services/fish.service';
 import { MatDialogRef } from '@angular/material/dialog';
+import { LevelService } from '../../../level/services/level.service';
 @Component({
   selector: 'app-add-fish',
   templateUrl: './add-fish.component.html',
@@ -10,13 +11,26 @@ export class AddFishComponent {
 
   errorMessage: string='';
   successMessage: string='';
+  levels: any[] = [];
 
   constructor(
     private fishService:FishService,
+    private levelService:LevelService,
     private dialogRef: MatDialogRef<AddFishComponent>
   ) { }
   
   closeLevelModal(){
     this.dialogRef.close();
   }
+  getAllLevels(){
+    this.levelService.getAllLevels().subscribe(
+      (response:any) => {
+        this.levels.push(...response.details.levels);
+      },
+      (error) => {
+        this.errorMessage=error.message;
+      }
+    );
+  }
+
 }
