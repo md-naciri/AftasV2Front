8974cdf6f7d968e6d7a24342935574ac8e16c884
md_naciri <med.ar.naciri@gmail.com>
2023-12-13 13:25:24 +0100

    add all necessary methods in fish component

--- src/app/modules/fishs/components/fish/fish.component.ts
+++ src/app/modules/fishs/components/fish/fish.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FishService } from '../../services/fish.service';
+import {MatDialog} from '@angular/material/dialog';
 
 @Component({
   selector: 'app-fish',
@@ -7,4 +9,31 @@ import { Component } from '@angular/core';
 })
 export class FishComponent {
 
+  isLoading: boolean = false;
+  fishes: any[]=[];
+  errorMessage: string='';
+  
+  constructor(
+    private fishService:FishService,
+    private dialog: MatDialog
+  ) { }
+  ngOnInit() {
+    this.isLoading=true;
+    this.getAllFishes();
+  }
+  getAllFishes(){
+    this.fishService.getAllFishes().subscribe(
+      (response:any) => {
+        this.fishes.push(...response.details.fishes);
+        this.isLoading=false;
+      },
+      (error) => {
+        this.errorMessage=error.message;
+        this.isLoading=false;
+      }
+    );
+  }
+  openAddFishModal(){
+    this.dialog.open(AddFishComponent);
+  }
 }
