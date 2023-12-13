b9874f2c633f9ee00d7872512c5f9c7a785fabc9
md_naciri <med.ar.naciri@gmail.com>
2023-12-13 13:27:34 +0100

    create the method to display all fishes fromdatabase

--- src/app/modules/fishs/services/fish.service.ts
+++ src/app/modules/fishs/services/fish.service.ts
@@ -1,9 +1,15 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from '../../../../environments/environment.deveopment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FishService {
 
-  constructor() { }
+  constructor(private httpClient:HttpClient) { }
+
+  getAllFishes(){
+    return this.httpClient.get(environment.apiURL+'fish');
+  }
 }
