caa206be3497e296c4761430653ad534c7093b9e
md_naciri <med.ar.naciri@gmail.com>
2023-12-15 15:40:46 +0100

    display all competition in select option of assing competition componenet

--- src/app/modules/members/components/assign-competition/assign-competition.component.ts
+++ src/app/modules/members/components/assign-competition/assign-competition.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
+import { CompetitionService } from '../../../competition/services/competition.service';
 
 @Component({
   selector: 'app-assign-competition',
@@ -11,10 +12,24 @@ export class AssignCompetitionComponent {
 
   errorMessage: string='';
   successMessage: string='';
+  competitions: any[] = [];
 
   constructor(
     private dialogRef: MatDialogRef<AssignCompetitionComponent>,
+    private competitionService: CompetitionService
   ) { }
+
+  ngOnInit() {
+    this.getAllCompetitions();
+  }
+  getAllCompetitions(){
+    this.competitionService.getAllCompetitions().subscribe(
+      (response:any)=>{
+         this.competitions.push(...response.details.Competitions);
+    },(error)=>{
+       console.log(error);
+    })
+  }
 
   closeAssignCompetitionModal() {
     this.dialogRef.close();
