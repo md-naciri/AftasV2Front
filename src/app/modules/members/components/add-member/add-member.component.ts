e2dd762b29ef675850d3190343f7e03bef1b66eb
md_naciri <med.ar.naciri@gmail.com>
2023-12-15 11:58:10 +0100

    add all ng model in add member component

--- src/app/modules/members/components/add-member/add-member.component.ts
+++ src/app/modules/members/components/add-member/add-member.component.ts
@@ -9,17 +9,60 @@ import { MatDialogRef } from '@angular/material/dialog';
 })
 export class AddMemberComponent {
 
+  member = {
+    firstName: '',
+    lastName: '',
+    identityNumber: '',
+    identityDocumentType: '',
+    nationality: ''
+  }
+
   errorMessage: string = '';
   successMessage: string = '';
+  details: string='';
   constructor(
-    private memberService:MemberService,
-    private dialogRef: MatDialogRef<AddMemberComponent> 
-    ) { }
+    private memberService: MemberService,
+    private dialogRef: MatDialogRef<AddMemberComponent>
+  ) { }
 
-  closeMemberModal(){
+  closeMemberModal() {
     this.errorMessage = '';
     this.successMessage = '';
     this.dialogRef.close();
+  }
+  saveMember() {
+    this.memberService.addMember(this.member).subscribe(
+      (data: any) => {
+        this.successMessage = data.message;
+        this.errorMessage = '';
+        this.member = {
+          firstName: '',
+          lastName: '',
+          identityNumber: '',
+          identityDocumentType: '',
+          nationality: ''
+        };
+      },
+      (err: any) => {
+        if (err.error.details) {
+          let detailsStr = '';
+          for (let key in err.error.details) {
+            if (err.error.details.hasOwnProperty(key)) {
+              detailsStr += err.error.details[key] + "\n";
+            }
+          }
+          this.details = detailsStr;
+        }
+        this.errorMessage = err.error.message + "\n  " + this.details;
+        this.successMessage = '';
+        this.member = {
+          firstName: '',
+          lastName: '',
+          identityNumber: '',
+          identityDocumentType: '',
+          nationality: ''
+        };
+      });
   }
 
 }
