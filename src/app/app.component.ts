8f57a95320da64d8024808280b13223dd28e1e21
md_naciri <med.ar.naciri@gmail.com>
2024-02-20 11:39:00 +0100

    edit displayed app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { AuthService } from './auth/auth.service';
+import {delay} from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,22 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'fish_competition_frontend';
+  title = 'Aftas';
+  showContent = true;
+  isLoading = true;
+  constructor(private router: Router,private authService: AuthService) {}
+  ngOnInit() {
+    this.router.events.pipe(
+      delay(1000)
+    ).subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.showContent = !['/login', '/register','/verify-mail'].includes(event.urlAfterRedirects);
+        this.isLoading = false;
+
+      }
+    });
+    this.authService.logoutEvent.subscribe(() => {
+      this.isLoading = true;
+    });
+  }
 }
