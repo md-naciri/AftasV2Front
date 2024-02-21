4a955644e6842661f0d11152aa71f2e7e992a63b
md_naciri <med.ar.naciri@gmail.com>
2024-02-21 17:12:25 +0100

    add logout method into navbar component

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../../auth/auth.service';
+import { UserService } from '../../../auth/user.service';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-navbar',
@@ -6,5 +10,26 @@ import { Component } from '@angular/core';
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
+  dropdownOpen = false;
+  userName: string ="";
+  constructor(private authService: AuthService,private userService: UserService, private router: Router,private toastr: ToastrService) {}
+  ngOnInit() {
+    this.userService.getFirstName().subscribe(name => {
+      this.userName = name;
+    });
+  }
+  logout() {
+    this.authService.logout().subscribe(() => {
+      localStorage.removeItem('accessToken');
+      this.router.navigate(['/login']);
+      this.toastr.success('Logout successful');
 
+    }, (error) => {
+      console.error('Error during logout:', error);
+    });
+  }
+  
+  toggleDropdown() {
+    this.dropdownOpen = !this.dropdownOpen;
+  }
 }
