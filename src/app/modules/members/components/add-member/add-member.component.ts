686b28c45890846ac4061ee017b9b7f54a810280
md_naciri <med.ar.naciri@gmail.com>
2024-02-22 15:46:58 +0100

    add email and feild password

--- src/app/modules/members/components/add-member/add-member.component.ts
+++ src/app/modules/members/components/add-member/add-member.component.ts
@@ -14,11 +14,62 @@ export class AddMemberComponent {
   member = {
     firstName: '',
     lastName: '',
+    email:'',
+    password:'',
     identityNumber: '',
-    identityDocumentType: '',
-    nationality: ''
+    identityDocumentType: 'PASSPORT',
+    nationality: 'ps'
   }
-
+  nationalities = [
+    {code: 'ps', name: 'Palestine'},
+    {code: 'ma', name: 'Morocco'},
+    {code: 'dz', name: 'Algeria'},
+    {code: 'us', name: 'United States'},
+    {code: 'ca', name: 'Canada'},
+    {code: 'fr', name: 'France'},
+    {code: 'de', name: 'Germany'},
+    {code: 'it', name: 'Italy'},
+    {code: 'es', name: 'Spain'},
+    {code: 'gb', name: 'United Kingdom'},
+    {code: 'au', name: 'Australia'},
+    {code: 'br', name: 'Brazil'},
+    {code: 'nl', name: 'Netherlands'},
+    {code: 'se', name: 'Sweden'},
+    {code: 'no', name: 'Norway'},
+    {code: 'ch', name: 'Switzerland'},
+    {code: 'at', name: 'Austria'},
+    {code: 'be', name: 'Belgium'},
+    {code: 'dk', name: 'Denmark'},
+    {code: 'fi', name: 'Finland'},
+    {code: 'ie', name: 'Ireland'},
+    {code: 'nz', name: 'New Zealand'},
+    {code: 'sg', name: 'Singapore'},
+    {code: 'jp', name: 'Japan'},
+    {code: 'kr', name: 'South Korea'},
+    {code: 'my', name: 'Malaysia'},
+    {code: 'mx', name: 'Mexico'},
+    {code: 'ph', name: 'Philippines'},
+    {code: 'pt', name: 'Portugal'},
+    {code: 'sa', name: 'Saudi Arabia'},
+    {code: 'tw', name: 'Taiwan'},
+    {code: 'th', name: 'Thailand'},
+    {code: 'ae', name: 'United Arab Emirates'},
+    {code: 'ar', name: 'Argentina'},
+    {code: 'cz', name: 'Czech Republic'},
+    {code: 'gr', name: 'Greece'},
+    {code: 'hu', name: 'Hungary'},
+    {code: 'pl', name: 'Poland'},
+    {code: 'ru', name: 'Russia'},
+    {code: 'tr', name: 'Turkey'},
+    {code: 'za', name: 'South Africa'},
+    {code: 'cn', name: 'China'},
+    {code: 'id', name: 'Indonesia'},
+    {code: 'vn', name: 'Vietnam'},
+    {code: 'eg', name: 'Egypt'},
+    {code: 'jo', name: 'Jordan'},
+    {code: 'lr', name: 'Liberia'},
+    
+  ];
   errorMessage: string = '';
   successMessage: string = '';
   details: string='';
@@ -40,6 +91,8 @@ export class AddMemberComponent {
         this.member = {
           firstName: '',
           lastName: '',
+          email:'',
+          password:'',
           identityNumber: '',
           identityDocumentType: '',
           nationality: ''
@@ -60,6 +113,8 @@ export class AddMemberComponent {
         this.member = {
           firstName: '',
           lastName: '',
+          email:'',
+          password:'',
           identityNumber: '',
           identityDocumentType: '',
           nationality: ''
